e206605ceab8c2e9e273d84337447bc458318820
Konrad <gladysz.konrad@gmail.com>
2023-04-07 16:10:38 +0100

    route changes

--- src/app/api/[id]/route.ts
+++
@@ -1,7 +0,0 @@
-export  async function GET(request: Request) {
-    const uri = `https://luasforecasts.rpa.ie/xml/get.ashx?action=forecast&stop=${request.id}encrypt=false`
-    const options = {
-        method: 'GET'
-    }
-    const res = await fetch(uri, options).then(data => console.log(data))
-}
